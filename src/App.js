1f3124859e3a9bfff2413fb648dde3e1c37acd6e
dedok16 <shestak16@gmail.com>
2021-08-12 12:41:33 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import './style.css';
 
 export default function App() {
   const num1 = 3;
-	const num2 = 2;
+  const num2 = 2;
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :) ???</p>	
-      <div>
-        result: {num1 ** num2}
-      </div>
+      <p>Start editing to see some magic happen :) ???</p>
+      <div>result: {num1 ** num2}</div>
     </div>
   );
 }
